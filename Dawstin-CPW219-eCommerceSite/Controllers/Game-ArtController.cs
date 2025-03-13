5a0ad69ba1f24529ddd3ed4d5a2d7e21ad4d7bc1
Dawstin Waters <dawstinw4@gmail.com>
2025-03-13 00:40:11 -0700

    Create game and painting fully functional, added some styling

--- Dawstin-CPW219-eCommerceSite/Controllers/Game-ArtController.cs
+++ Dawstin-CPW219-eCommerceSite/Controllers/Game-ArtController.cs
@@ -1,13 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dawstin_CPW219_eCommerceSite.Game_Art_Data;
+using Dawstin_CPW219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dawstin_CPW219_eCommerceSite.Controllers
 {
     public class Game_ArtController : Controller
     {
+        private readonly VideoGame_Art _context;
+
+        public Game_ArtController(VideoGame_Art context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Game_Art game_Art)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Game_Arts.Add(game_Art); // Prepares insert
+                _context.SaveChanges(); // Excecutes pending insert
+
+                ViewData["Message"] = $"{game_Art.Title} was added successfully!";
+                return View();
+            }
+
+            return View(game_Art);
         }
     }
 }
