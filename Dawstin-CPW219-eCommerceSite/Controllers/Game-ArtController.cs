6b0d1830dac2e4015c5d541b408b1829f6d16f8a
Dawstin Waters <dawstinw4@gmail.com>
2025-03-13 01:23:40 -0700

    Refactored add game/painting code to be asynchronous

--- Dawstin-CPW219-eCommerceSite/Controllers/Game-ArtController.cs
+++ Dawstin-CPW219-eCommerceSite/Controllers/Game-ArtController.cs
@@ -20,12 +20,14 @@ namespace Dawstin_CPW219_eCommerceSite.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Game_Art game_Art)
+        public async Task<IActionResult> Create(Game_Art game_Art)
         {
             if (ModelState.IsValid)
             {
-                _context.Game_Arts.Add(game_Art); // Prepares insert
-                _context.SaveChanges(); // Excecutes pending insert
+                _context.Game_Arts.Add(game_Art);   // Prepares insert
+                // For async code info in the tutorial
+                // https://learn.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro?view=aspnetcore-8.0&tabs=visual-studio
+                await _context.SaveChangesAsync();  // Excecutes pending insert
 
                 ViewData["Message"] = $"{game_Art.Title} was added successfully!";
                 return View();
